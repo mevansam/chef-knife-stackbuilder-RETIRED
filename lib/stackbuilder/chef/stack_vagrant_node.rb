f4dbeb3c1e76d29d8a3f61fffd3dd6cc97ae99a3
Mevan Samaratunga <mevansam@gmail.com>
2014-12-23 16:15:50 -0500

    moved vmdk extra disk creation to stack provider extension

--- lib/stackbuilder/chef/stack_vagrant_node.rb
+++ lib/stackbuilder/chef/stack_vagrant_node.rb
@@ -8,6 +8,8 @@ module StackBuilder::Chef
 
         def create_vm(name, knife_config)
 
+            handle_vagrant_box_additions(name, knife_config)
+
             knife_cmd = Chef::Knife::VagrantServerCreate.new
 
             knife_cmd.config[:chef_node_name] = name
@@ -53,6 +55,8 @@ module StackBuilder::Chef
                 run_knife(knife_cmd, 3)
             }
 
+            handle_vagrant_box_cleanup(knife_config)
+
         rescue Exception => msg
 
             if Dir.exist?(knife_cmd.config[:vagrant_dir] + '/' + name)
@@ -67,6 +71,81 @@ module StackBuilder::Chef
                     FileUtils.rm_rf(knife_cmd.config[:vagrant_dir] + '/' + name)
                 end
             end
+        end
+
+        def handle_vagrant_box_additions(name, knife_config)
+
+            # Create add-on provider specific infrastructure
+            knife_options = knife_config['options']
+            provider = knife_options['provider']
+
+            if provider.start_with?('vmware')
+
+                vmx_customize = knife_options['vmx_customize']
+                unless vmx_customize.nil?
+
+                    # Build additional disks that will be added to
+                    # the VMware fusion/desktop VM when booted.
+
+                    disks = {}
+                    vagrant_disk_path = File.join(Dir.home, '/.vagrant/disks') + '/' + name
+                    FileUtils.mkdir_p(vagrant_disk_path)
+
+                    vmx_customize.split(/::/).each do |p|
+
+                        kv = p.split('=')
+                        k = kv[0].gsub(/\"/, '').strip
+                        v = kv[1].gsub(/\"/, '').strip
+
+                        if k.start_with?('scsi')
+                            ss = k.split('.')
+                            disks[ss[0]] ||= {}
+                            case ss[1]
+                                when 'fileName'
+                                    if v.start_with?('/')
+                                        disks[ss[0]]['fileName'] = v
+                                    else
+                                        vv = vagrant_disk_path + '/' + v
+                                        vmx_customize.gsub!(/#{v}/, vv)
+                                        disks[ss[0]]['fileName'] = vv
+                                    end
+                                when 'fileSize'
+                                    disks[ss[0]]['fileSize'] = v
+                            end
+                        end
+                    end
+
+                    # Create extra disks as unlike virtual box VMware fusion/workstation
+                    # will not create disks automatically based on configuration params
+
+                    vdiskmgr = %x(which vmware-vdiskmanager)
+                    vdiskmgr = "/Applications/VMware Fusion.app/Contents/Library/vmware-vdiskmanager" \
+                        if is_os_x? && vdiskmgr.empty?
+
+                    if File.exist?(vdiskmgr)
+
+                        job_handles = run_jobs(disks.values) do |f|
+
+                            disk = f['fileName']
+                            @logger.info("Creating disk #{disk}.")
+
+                            %x("#{vdiskmgr}" -c -t 0 -s #{f['fileSize']} -a ide #{disk}) \
+                                unless File.exist?(f['fileName'])
+
+                            raise StackBuilder::Common::StackBuilderError, "Disk #{disk} could not be created." \
+                                unless File.exist?(disk)
+                        end
+                        job_results = wait_jobs(job_handles)
+                    else
+                        raise StackBuilder::Common::StackBuilderError,
+                            "Unable to determine path to vmware-vdiskmanager" +
+                            "to create the requested additional disk."
+                    end
+                end
+            end
+        end
+
+        def handle_vagrant_box_cleanup(knife_config)
         end
     end
 end
